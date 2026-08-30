35767c2247689fdb1077c793a46f29c23ad84c17
Martin Carcano <martin.carcano342@gmail.com>
2026-08-29 23:45:30 -0300

    Creo la carpeta 05 Listas y subo el TP

---
+++ 05 Listas/Carcano_TP-05.py
@@ -0,0 +1,60 @@
+#Ejercicio 1
+lista = list(range(4,101,4))
+print(lista)
+
+
+#Ejercicio 2 
+lista = [1 , 2 , 3 , 4 , 5]
+print(lista[-2])
+
+
+#Ejercicio 3 
+lista = []
+lista.append(1)
+lista.append(2)
+lista.append(3)
+print(lista)
+
+
+#Ejercicio 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales [1] = "loro"
+animales [-1] = "oso"
+print(animales)
+
+
+#Ejercicio 5 
+#Lo que se realiza es que se define una lista con varios numeros, el numeros.remove elimina un elemento de la lista, el max(numeros) saca el numero mas alto que este en la variable numeros, y despues imprime por pantalla. En resumen elimina el numero mas alto de la lista.
+
+
+#Ejercicio 6
+lista = list(range(10,31,5))
+print(lista[:2])
+
+
+#Ejercicio 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "bora"
+autos[2] = "vento"
+print(autos)
+
+
+#Ejercicio 8
+dobles = []
+dobles.append(10)
+dobles.append(20)
+dobles.append(30)
+print(dobles)
+
+
+#Ejercicio 9
+compras = [["pan", "leche"] , ["arroz", "fideos", "salsa"] , ["agua"]]
+compras[2].append("jugo")
+compras [1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+
+#Ejercicio 10
+lista_anidada = [[15] , [True] , [25.5, 57.59, 30.6] , [False]]
+print(lista_anidada)
